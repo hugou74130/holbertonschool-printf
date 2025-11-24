220e965a44aa37864acb3a0cc4a1d6d423e725ce
Hugo <hugoramos1466@gmail.com>
2025-11-24 07:26:36 +0100

    structure of the main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(char *s);
+int print_percent(void);
+int print_int(int n);
+
+#endif
