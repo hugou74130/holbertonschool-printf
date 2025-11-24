ca555763db0ff9c78e4b3b4138602ae505b812d5
SaraEstelle <12152@holbertonstudents.com>
2025-11-24 14:48:20 +0100

    Add print_char function to handle %c specifier

--- print_char.c
+++ print_char.c
@@ -1,5 +1,19 @@
 #include "main.h"
 
-int print_char(char c)
+/**
+ * print_char - print a character
+ * @args: va_list containing the character to print
+ *
+ * Return: number of characters printed (1)
+ */
+
+int print_char(va_list args)
 {
+ char c;
+ c = va_arg(args, int);
+
+ if (write(1, &c, 1) == -1)
+		return (-1);
+
+ return (1);
 }
