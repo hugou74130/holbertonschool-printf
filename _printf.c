59b2870eac719bfc082d87887c6662d25a3a3962
Hugo <hugoramos1466@gmail.com>
2025-11-25 10:50:09 +0100

    FIX _printf.c

--- _printf.c
+++ _printf.c
@@ -3,41 +3,44 @@
 
 int _printf(const char *format, ...)
 {
- va_list args;
- int i = 0;
- int count = 0;
- int (*func)(va_list);
+	va_list args;
+	int i = 0;
+	int count = 0;
+	int (*func)(va_list);
 
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
 
-if (format == NULL)
- return (-1);
-va_start(args, format);
-
-while (format[i])
-{
- if (format[i] == '%')
-		i++;
- if (format[i]== '\0')
+	while (format[i])
+	{
+		if (format[i] == '%')
 		{
-			va_end(args);
-			return (-1);
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			func = get_specifier(format[i]);
+			if (func)
+				count += func(args);
+			else
+			{
+				_puthcar('%');
+				_puctahr(format[i]);
+				count += 2;
+			}
 		}
- func = get_specifier(format[i]);
- if (func)
-		count += func(args);
- else
- {
-		_puthcar('%');
-		_puctahr(format[i]);
-		count +=2;
+		else
+		{
+			_putchar(format[i]);
+			count++;
+			{
+				i++;
+			}
+			va_end(args);
+			return (count);
+		}
 	}
- 
- {
-		_putchar(format[i]);
-		count++;
- }
- i++;
-}
-va_end(args);
-return (count);
 }
