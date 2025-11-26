3dde0e77f54ee342c16bb64e4359ac5bd63c34c7
Hugo <hugoramos1466@gmail.com>
2025-11-26 14:12:11 +0100

    advanced task complete

---
+++ print_hex_upper.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int print_unsigned(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	char *symbols = "01234567ABCDEF";
+	return (print_unsigned_base(n, 16, symbols));
+}
