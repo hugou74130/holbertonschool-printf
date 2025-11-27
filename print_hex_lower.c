a2830d9fb62566f269def80770a6467270c26e30
Hugo <hugoramos1466@gmail.com>
2025-11-27 11:16:11 +0100

    advanced task

--- print_hex_lower.c
+++ print_hex_lower.c
@@ -3,6 +3,6 @@
 int print_hex_lower(va_list args)
 {
 	unsigned int n = va_arg(args, unsigned int);
-	char *symbols = "01234567abcdef";
+	char *symbols = "0123456789abcdef";
 	return (print_unsigned_base(n, 16, symbols));
 }
