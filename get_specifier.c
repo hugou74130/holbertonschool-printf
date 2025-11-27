abbf903d1d461aa5752f39ae9c0fdf1c47ee94ff
Hugo <hugoramos1466@gmail.com>
2025-11-27 16:27:59 +0100

    adding comments on the file get_specifier

--- get_specifier.c
+++ get_specifier.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * get_specifier - Cherche la fonction appropriée pour un spécificateur de format donné.
+ * @c: Le caractère spécificateur de format (ex: 'd', 'i', 'c', 's', '%').
+ *
+ * Cette fonction parcourt un tableau de structures pour trouver une correspondance
+ * entre le caractère spécificateur de format passé en argument et une fonction
+ * d'impression associée (handler).
+ *
+ * Return: Un pointeur vers la fonction d'impression (handler) qui correspond au
+ * spécificateur 'c', ou NULL si aucune correspondance n'est trouvée.
+ */
+
 int (*get_specifier(char c))(va_list)
 {
 	specifier_t specifiers[] = {
